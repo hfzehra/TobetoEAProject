097320c7b9e09215a336ad469117515fd9b49fb2
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2023-11-11 18:26:09 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _inProductDal;
+
+        public ProductManager(IProductDal inProductDal)
+        {
+            _inProductDal = inProductDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //iş kodları
+            //Yetkisi var mı ? kontrolü
+            return _inProductDal.GetAll();
+            
+        }
+    }
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //Generic Constraint
+    //class  :referans tip olur demek 
+    //new() işlemi  yapabilir
+    public interface IEntityRepository<T> where T : class,IEntity,new()
+    {
+        //Tekrarlayan işlemleri tek bir yerden çekiyoruz 
+
+        List<T> GetAll(Expression<Func<T,bool>> filter=null); //select
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity); //insert
+        void Update(T entity);
+        void Delete(T entity);
+
+        //Yukarıdaki expression yapısıı ile aşağıdaki kodu sağlayabiliriz.
+        //List<T> GetAllByCategory(int categoryId);
+    }
+}
