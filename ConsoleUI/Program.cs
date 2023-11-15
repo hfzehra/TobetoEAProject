a79decc3e7a423bd55fea649dcde0b5d5e9bf4d9
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2023-11-15 11:46:35 +0300

    Core katmanı ve DTO üzerinde çalışıldı

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 
 namespace ConsoleUI
 {
@@ -8,8 +9,9 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //Data Transformation Object
             //Open Closed Principle
-            //ProductTest();
+            ProductTest();
             //CategoryTest();
         }
 
@@ -27,9 +29,9 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + "  -->    " + product.CategoryName);
             }
         }
     }
